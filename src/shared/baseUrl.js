db3c5e129a776c29ceb14216a0e9df9e45c66780
Tekie79 <76453162+Tekie79@users.noreply.github.com>
2021-07-01 22:33:31 -0400

    Update baseUrl.js

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -6,10 +6,10 @@ req.onreadystatechange = () => {
   }
 };
 
-req.open("GET", "https://api.jsonbin.io/b/60de5a7f55b7245a20d37692/latest", true);
+
 req.setRequestHeader("X-Master-Key", "$2b$10$Q0EK6m9jRYgJmxstdCyFCO9tdLur8aHXnaMu9TKx58rNYj16cNOeq");
+ req.send();
 
 
 
-
-export const baseUrl = req.send();
+export const baseUrl = req.open("GET", "https://api.jsonbin.io/b/60de5a7f55b7245a20d37692/latest/", true);
