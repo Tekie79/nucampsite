975ea7365587c3da8369f7873aced38a3385fb94
Tekie79 <76453162+Tekie79@users.noreply.github.com>
2021-07-01 22:17:15 -0400

    json server request

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,15 @@
-export const baseUrl = 'http://localhost:3001/';
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+  if (req.readyState == XMLHttpRequest.DONE) {
+    console.log(req.responseText);
+  }
+};
+
+req.open("GET", "https://api.jsonbin.io/b/60de5a7f55b7245a20d37692/1", true);
+req.setRequestHeader("X-Master-Key", "$2b$10$Q0EK6m9jRYgJmxstdCyFCO9tdLur8aHXnaMu9TKx58rNYj16cNOeq");
+
+
+
+
+export const baseUrl = req.send();
