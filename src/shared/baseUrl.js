2bea0d7945083600d633fdc69700ac8e61eea9ec
Tekie79 <76453162+Tekie79@users.noreply.github.com>
2021-07-01 22:50:14 -0400

    Update baseUrl.js

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,15 +1,1 @@
-let req = new XMLHttpRequest();
-
-req.onreadystatechange = () => {
-  if (req.readyState == XMLHttpRequest.DONE) {
-    console.log(req.responseText);
-  }
-};
-
-req.open("GET", "https://api.jsonbin.io/b/60de5a7f55b7245a20d37692/latest/", true);
-req.setRequestHeader("X-Master-Key", "$2b$10$Q0EK6m9jRYgJmxstdCyFCO9tdLur8aHXnaMu9TKx58rNYj16cNOeq");
-
-
-
-
-export const baseUrl =  req.send();
+export const baseUrl = 'http://localhost:3001/';
