0492cd89b5be5706feb3fd5172818a47fd2f1f42
Tekie79 <76453162+Tekie79@users.noreply.github.com>
2021-07-02 00:36:59 -0400

    image path

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -17,7 +17,7 @@ function RenderCard({ item, isLoading, errMess }) {
       transformProps={{ exitTransform: "scale(0.5) translateY(50%)" }}
     >
       <Card>
-        <CardImg src={baseUrl + item.image} alt={item.name} />
+        <CardImg src={item.image} alt={item.name} />
         <CardBody>
           <CardTitle>{item.name}</CardTitle>
           <CardText>{item.description}</CardText>
